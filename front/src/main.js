44efacdc3fe159503a462f623616ab4e9b16dcb9
Diogo Andrade <da7822000@gmail.com>
2026-08-20 11:04:24 +0200

    Ajout de la variable version

--- front/src/main.js
+++ front/src/main.js
@@ -5,6 +5,8 @@ import router from './router'
 import { initializeFavorites } from '@/stores/favorites'
 import { initializeTheme } from '@/stores/theme'
 
+export const APP_VERSION = '1.0.2'
+
 const startApp = async () => {
   await Promise.all([
     initializeFavorites(),
